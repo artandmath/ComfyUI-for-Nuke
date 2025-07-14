4441d0b4940d6f6f585580ff001098b5d512b0e5
Daniel H. <danielharkness@mac.com>
2025-07-15 08:55:52 +1000

    Update env.py
    Allows for env vars to be modified whilst nuke is operating

--- env.py
+++ env.py
@@ -1,14 +1,29 @@
 import os
 from .nuke_util.nuke_util import get_nuke_path
 
-dir_local  = '<path_TO_shared_ComfyUI>'  #eg '//10.10.10.10/shares/ComfyUI'
-dir_remote = '<path_ON_shared_ComfyUI>'  #eg '/home/user/ComfyUI/'
-ip         = '10.10.10.10'
-port       = 8188
-nuke_user  = get_nuke_path() #/home/<USER>/.nuke
+# Default values
+_dir_local  = '<path_TO_shared_ComfyUI>'  #eg '//10.10.10.10/shares/ComfyUI'
+_dir_remote = '<path_ON_shared_ComfyUI>'  #eg '/home/user/ComfyUI/'
+_ip         = '10.10.10.10'
+_port       = 8188
+_nuke_user  = get_nuke_path() #/home/<USER>/.nuke
 
-NUKE_COMFYUI_DIR_LOCAL = os.environ.get('NUKE_COMFYUI_DIR_LOCAL', dir_local)
-NUKE_COMFYUI_DIR_REMOTE = os.environ.get('NUKE_COMFYUI_DIR_REMOTE', dir_remote)
-NUKE_COMFYUI_IP = os.environ.get('NUKE_COMFYUI_IP',ip)
-NUKE_COMFYUI_PORT = int(os.environ.get('NUKE_COMFYUI_PORT', port))
-NUKE_COMFYUI_NUKE_USER = os.environ.get('NUKE_COMFYUI_NUKE_USER', nuke_user)
+def NUKE_COMFYUI_DIR_LOCAL():
+    """Get local ComfyUI directory from environment or default"""
+    return os.environ.get('NUKE_COMFYUI_DIR_LOCAL', _dir_local)
+
+def NUKE_COMFYUI_DIR_REMOTE():
+    """Get remote ComfyUI directory from environment or default"""
+    return os.environ.get('NUKE_COMFYUI_DIR_REMOTE', _dir_remote)
+
+def NUKE_COMFYUI_IP():
+    """Get ComfyUI IP from environment or default"""
+    return os.environ.get('NUKE_COMFYUI_IP', _ip)
+
+def NUKE_COMFYUI_PORT():
+    """Get ComfyUI port from environment or default"""
+    return int(os.environ.get('NUKE_COMFYUI_PORT', _port))
+
+def NUKE_COMFYUI_NUKE_USER():
+    """Get Nuke user directory from environment or default"""
+    return os.environ.get('NUKE_COMFYUI_NUKE_USER', _nuke_user)
